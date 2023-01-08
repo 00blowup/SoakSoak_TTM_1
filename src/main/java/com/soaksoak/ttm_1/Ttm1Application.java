309b734900bb7bde9334e1efe368126e4f2f93f7
Haeun Noh <00blowup@gmail.com>
2023-01-09 03:43:30 +0900

    Initial commit

---
+++ src/main/java/com/soaksoak/ttm_1/Ttm1Application.java
@@ -0,0 +1,13 @@
+package com.soaksoak.ttm_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ttm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ttm1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/soaksoak/ttm_1/Ttm1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soaksoak.ttm_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ttm1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
